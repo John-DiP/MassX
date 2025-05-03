5a645252b348586136eda3dacf470ee14c84b373
John D. <103323388+John-DiP@users.noreply.github.com>
2025-05-02 20:26:23 -0500

    Add files via upload

--- MassX/src/Vector.java
+++ MassX/src/Vector.java
@@ -3,18 +3,21 @@ This class deals with vectors, which is a quantity that represents magnitude and
 */
 
 public class Vector {
-	private double vectorX;
-	private double vectorY;
+	private double X; // represents horizontal
+	private double Y; // represents vertical
 	
 	public Vector() {
-		this.vectorX = 0;
-		this.vectorY = 0;
+		this.X = 0;
+		this.Y = 0;
 	}
 	
 	public Vector(double x, double y) {
-		this.vectorX = x;
-		this.vectorY = y;
+		this.X = x;
+		this.Y = y;
 	}
 	
+	public double add() {
+		
+	}
 	
 }
